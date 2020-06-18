3a52a3502b94fe86d3d118b33d67c59b0207fded
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-18 11:20:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20200618111926) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers_taxis", id: false, force: :cascade do |t|
+    t.integer "taxi_id",      null: false
+    t.integer "passenger_id", null: false
   end
 
   create_table "rides", force: :cascade do |t|
